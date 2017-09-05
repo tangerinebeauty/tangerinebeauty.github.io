3851fe4f72a1289941612cbc30b289f83083039b
Kiky Tangerine <kwtangerine@gmail.com>
2017-09-05 01:59:10 -0700

    Use cdnjs instead of vendor

--- gulpfile.js
+++ gulpfile.js
@@ -46,37 +46,8 @@ gulp.task('minify-js', function() {
     }))
 });
 
-// Copy vendor files from /node_modules into dist/vendor
-// NOTE: requires `npm install` before running!
 // Copy other static assets to dist
 gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('dist/vendor/bootstrap'));
-
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('dist/vendor/jquery'));
-
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('dist/vendor/popper'));
-
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('dist/vendor/jquery-easing'));
-
-  gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest('dist/vendor/font-awesome'));
-
   // Copy img
   gulp.src([
         'img/**',
